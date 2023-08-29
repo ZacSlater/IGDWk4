ab6036fec660a1b0dd29fea97d7b45877642fbba
ZacSlater <zachary.slater@student.uts.edu.au>
2023-08-29 15:23:20 +1000

    Update PrintAndHide.cs

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -16,5 +16,17 @@ public class PrintAndHide: MonoBehaviour
     void Update()
     {
         Debug.Log(gameObject.name + ":" + ++i);
+
+        if (gameObject.tag == "Red" && i == 100)
+        {
+            gameObject.SetActive(false);
+        }
+
+        int a = Random.Range(150, 250);
+        if (gameObject.tag == "Blue" && i == a)
+        {
+            rend.enabled = false;
+        }
+
     }
 }
